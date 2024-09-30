037e1cce0545ee89631d9b24ee129b10010284e3
ingerstep <stepan.karelin.1996@mail.ru>
2024-09-30 17:48:45 +0700

    update(docker): update env & node alpine v

--- server/src/config/typeorm.ts
+++ server/src/config/typeorm.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
-dotenvConfig({ path: '.env' });
+dotenvConfig({ path: '../.env' });
 
 const config = {
   type: 'postgres',
